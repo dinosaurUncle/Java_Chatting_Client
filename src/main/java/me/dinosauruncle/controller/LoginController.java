96fce829f8c3139a3f569e2546b53e3214e16ed3
박 종훈 <m05214@naver.com>
2020-02-27 23:10:10 +0900

    회원가입 아이디 사용여부 기능구현

--- src/main/java/me/dinosauruncle/controller/LoginController.java
+++ src/main/java/me/dinosauruncle/controller/LoginController.java
@@ -5,6 +5,7 @@ import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import me.dinosauruncle.common.DataStructureConvert;
@@ -29,7 +30,7 @@ public class LoginController {
     private TextField loginId;
 
     @FXML
-    private TextField loginPw;
+    private PasswordField loginPw;
 
     @FXML
     private Button loginButton;
